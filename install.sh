dafca1e519119d16fdb6d6666cbcb54e568120ac
BladeCarrier <sasha_panin@mail.ru>
2016-02-20 14:57:20 +0300

    add lost dependencies: pymongo

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ sudo apt-get install -y libxslt1-dev python3-pip
 
 pip install --upgrade pip
 pip3 install --upgrade pip3
-pip install --upgrade nltk openpyxl pymorphy2 plotly bokeh
+pip install --upgrade pymongo nltk openpyxl pymorphy2 plotly bokeh
 
