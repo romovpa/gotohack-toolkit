fa95f25fb42b69bcb9c4e72a0493ae18ff008543
Peter Romov <peter@romov.ru>
2016-02-20 10:47:51 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install -y libxml2-dev libxslt1-dev python3-pip 
+
+pip install --upgrade pip
+pip3 install --upgrade pip3
+pip install --upgrade nltk lxml openpyxl pymorphy2 plotly bokeh
+
