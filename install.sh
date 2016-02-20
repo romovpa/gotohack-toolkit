4a189b1306618c2d354fa17da2b2269ad45d8ddb
BladeCarrier <sasha_panin@mail.ru>
2016-02-20 14:39:35 +0300

    remove lxml: not required

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y libxml2-dev libxslt1-dev python3-pip 
+sudo apt-get install -y libxslt1-dev python3-pip 
 
 pip install --upgrade pip
 pip3 install --upgrade pip3
-pip install --upgrade nltk lxml openpyxl pymorphy2 plotly bokeh
+pip install --upgrade nltk openpyxl pymorphy2 plotly bokeh
 
