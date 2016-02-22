ea856d5531168e5f34440f52d9ae39c4d8b20b8c
BladeCarrier <sasha_panin@mail.ru>
2016-02-22 05:26:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,4 +20,5 @@ conda install -y nltk
 git clone https://github.com/plotly/plotly.py
 cd plotly.py && python setup.py build install && cd ..
 
-
+git clone https://github.com/kmike/pymorphy2
+cd pymorphy2 && python setup.py build install && cd ..
